91b44c959f4568698f7528e6e17dfecf2e375968
Fernando Rojas <rojasdsa@alumnos.ilernasevilla.com>
2024-01-11 11:18:31 +0100

    añadida funcionalidad icono

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,1 +1,17 @@
 'use strict'
+
+/* ----------------------------------------------------------*/
+/* Agitar el icono de git y al hacer click te lleva a github */
+
+function agitarAutomaticamente() {
+    let gitLogo = document.getElementById('gitLogo');
+    gitLogo.classList.add('agitacion');
+
+    // Elimina la clase de agitación después de la duración de la animación
+    setTimeout(function() {
+        gitLogo.classList.remove('agitacion');
+    }, 500);
+}
+
+// Agitar automáticamente cada 3 segundos (3000 milisegundos)
+setInterval(agitarAutomaticamente, 3000);
