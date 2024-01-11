0e8a31524094bdf4348684bf8484e73cff24cb64
Fernando Rojas <rojasdsa@alumnos.ilernasevilla.com>
2024-01-11 11:03:35 +0100

    correción

--- index.php
+++ index.php
@@ -3,14 +3,11 @@
 
 <main class="container-fluid">
     <div class="row">
-
         <!-- Contenido principal -->
-        <main class="text-center">
             <article>
                 <h2 class="text-center mt-4">Bienvenido a la Página de Inicio</h2>
                 <p class="text-center">Web libre con php y docker</p>
             </article>
-        </main>
     </div>
 </main>
 
