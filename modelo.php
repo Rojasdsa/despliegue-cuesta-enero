b6385326acfcd3015ffccac5115bda0b4ff6580a
Fernando Rojas <rojasdsa@alumnos.ilernasevilla.com>
2024-01-12 11:18:19 +0100

    cambio localhost -> 'db'

--- modelo.php
+++ modelo.php
@@ -3,7 +3,7 @@ class ConexionBD
 {
     // Si se abre con docker cambiar el $host a ''db
     // Si se abre con xampp cambiar el $host a 'localhost'
-    private string $host = 'localhost';
+    private string $host = 'db';
     private string $usuario = 'root';
     private string $contrasena = '';
     private string $nombre_bd = 'tu_basededatos';
