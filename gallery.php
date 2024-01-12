0addef883ce0cde49206eafa34fb33de8568a024
Fernando Rojas <rojasdsa@alumnos.ilernasevilla.com>
2024-01-12 01:28:47 +0100

    galería finalizada

---
+++ gallery.php
@@ -0,0 +1,50 @@
+<!-- Header que incluye desde el DOCTYPE hasta la etiqueta <header> -->
+<?php include('includes/header.php'); ?>
+
+<main class="container">
+
+    <h3 class="text-center text-white bg-dark mt-3 rounded-3">Cosas con las que programo</h3>
+
+    <div class="container gallery">
+        <div class="image p-3">
+            <img src="resources/img/gallery/bootstrap.png" alt="Imagen 1">
+        </div>
+        <div class="image p-3">
+            <img src="resources/img/gallery/css.png" alt="Imagen 2">
+        </div>
+        <div class="image p-3">
+            <img src="resources/img/gallery/docker.png" alt="Imagen 3">
+        </div>
+        <div class="image p-3">
+            <img src="resources/img/gallery/html.png" alt="Imagen 4">
+        </div>
+        <div class="image p-3">
+            <img src="resources/img/gallery/java.png" alt="Imagen 5">
+        </div>
+        <div class="image p-3">
+            <img src="resources/img/gallery/js.png" alt="Imagen 6">
+        </div>
+        <div class="image p-3">
+            <img src="resources/img/gallery/laravel.png" alt="Imagen 7">
+        </div>
+        <div class="image p-3">
+            <img src="resources/img/gallery/mysql.png" alt="Imagen 8">
+        </div>
+        <div class="image p-3">
+            <img src="resources/img/gallery/node.png" alt="Imagen 9">
+        </div>
+        <div class="image p-3">
+            <img src="resources/img/gallery/php.png" alt="Imagen 10">
+        </div>
+        <div class="image p-3">
+            <img src="resources/img/gallery/react.png" alt="Imagen 11">
+        </div>
+        <div class="image p-3">
+            <img src="resources/img/gallery/sass.png" alt="Imagen 12">
+        </div>
+    </div>
+
+</main>
+
+<!-- Footer que incluye desde <footer> hasta </body> y </html> -->
+<?php include('includes/footer.php'); ?>
