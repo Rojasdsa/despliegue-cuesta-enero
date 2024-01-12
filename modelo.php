64ad1a4c5706b691ded827d060115cba0cd13dfa
Fernando Rojas <rojasdsa@alumnos.ilernasevilla.com>
2024-01-12 11:15:52 +0100

    add comentario docker/xampp

--- modelo.php
+++ modelo.php
@@ -1,7 +1,9 @@
 <?php
 class ConexionBD
 {
-    private string $host = 'db';
+    // Si se abre con docker cambiar el $host a ''db
+    // Si se abre con xampp cambiar el $host a 'localhost'
+    private string $host = 'localhost';
     private string $usuario = 'root';
     private string $contrasena = '';
     private string $nombre_bd = 'tu_basededatos';
