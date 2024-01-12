e6a140c3d9ea9185b711481a5ca173f0fb08d463
Fernando Rojas <rojasdsa@alumnos.ilernasevilla.com>
2024-01-12 11:16:07 +0100

    lista tecnologías inicio

--- index.php
+++ index.php
@@ -3,8 +3,40 @@
 
 <main class="container-fluid">
 
-
-  
+    <h3 class="text-center text-white bg-dark mt-3 rounded-3">Software utilizado</h3>
+    <ul class="list-group">
+        <li class="list-group-item rounded-2 custom-hover">
+        <a class="text-decoration-none text-dark" href="https://code.visualstudio.com/" target="_blank">Visual Studio Code - 1.85.1</a>
+        </li>
+        <ul class="list-group rounded-2">
+            <li class="list-group-item rounded-2 ms-5 custom-hover">
+            <a class="text-decoration-none text-dark" href="https://www.php.net/" target="_blank">PHP - 8.3.1</a>
+            </li>
+            <li class="list-group-item rounded-2 ms-5 custom-hover border">
+            <a class="text-decoration-none text-dark" href="https://getbootstrap.com/" target="_blank">Bootstrap - 5.3</a>
+            </li>
+        </ul>
+        <li class="list-group-item rounded-2 custom-hover">
+        <a class="text-decoration-none text-dark" href="https://www.mysql.com/" target="_blank">MySQL Workbench - 8.0.31</a>
+        </li>
+        <li class="list-group-item rounded-2 custom-hover border">
+        <a class="text-decoration-none text-dark" href="https://www.gitkraken.com/" target="_blank">GitKraken - 9.11.1</a>
+        </li>
+        <ul class="list-group">
+            <li class="list-group-item rounded-2 ms-5 custom-hover">
+            <a class="text-decoration-none text-dark" href="https://github.com/" target="_blank">GitHub</a>
+            </li>
+        </ul>
+        <li class="list-group-item rounded-2 custom-hover">
+        <a class="text-decoration-none text-dark" href="https://drive.google.com/" target="_blank">Google Drive</a>
+        </li>
+        <li class="list-group-item rounded-2 custom-hover border">
+        <a class="text-decoration-none text-dark" href="https://www.docker.com/" target="_blank">Docker - 24.0.6</a>
+        </li>
+        <li class="list-group-item rounded-2 custom-hover border">
+        <a class="text-decoration-none text-dark" href="https://www.apachefriends.org/index.html" target="_blank">XAMPP</a>
+        </li>
+    </ul>
 
 </main>
 
